73ce8b1274f4eeead1295c342354ce40030232fd
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2020-12-19 14:37:16 -0300

    Make video accept longer youtube urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def embbed_url(video)
-    "https://www.youtube.com/embed/#{video.youtube_link.split('/').last}"
+    "https://www.youtube.com/embed/#{video.youtube_link.split('/').last.split("=").last}"
   end
 end

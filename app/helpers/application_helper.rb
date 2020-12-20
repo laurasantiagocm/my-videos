8c07d4c5c01e281862c21d4aa3860583daee8e34
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2020-12-19 23:22:34 -0300

    Add iziToast notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,26 @@ module ApplicationHelper
   def embbed_url(video)
     "https://www.youtube.com/embed/#{video.youtube_link.split('/').last.split("=").last}"
   end
+
+  def flash_tag
+    messages = []
+
+    flash.each do |type, message|
+      next if message.blank?
+
+      level = case type
+              when "notice" then "success"
+              when "alert"  then "error"
+              when "info"   then "info"
+              when "warn"   then "warn"
+              else
+                "info"
+              end
+
+      js = %[iziToast.#{level}({ title: '#{level.capitalize}:', message: "#{message}" });]
+      messages << javascript_tag(js)
+    end
+
+    messages.join("/n").html_safe
+  end
 end
